8c0d1b89bc28a3a6fce7a69c295a4578e00e28a3
Tangjj1996 <awesometangji@gmail.com>
2023-10-30 08:27:24 +0800

    feat: Wechaty build

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { WechatyBuilder } from "wechaty";
+import QRCode from "qrcode";
+
+const bot = WechatyBuilder.build({
+  name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
+  puppet: "wechaty-puppet-wechat",
+  puppetOptions: {
+    uos: true,
+  },
+});
+
+bot
+  .on("scan", async (qrcode, status) => {
+    console.log(
+      `Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(
+        qrcode,
+      )}`,
+    );
+    console.log(
+      await QRCode.toString(qrcode, { type: "terminal", small: true }),
+    );
+  })
+  .on("login", (user) => console.log(`User ${user} logged in`))
+  .on("message", (message) => console.log(`Message: ${message.text()}`));
+await bot.start();
