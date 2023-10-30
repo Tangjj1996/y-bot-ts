83635a8d51b28e79291d1d13b8eecb3f1bd50f82
Tangjj1996 <awesometangji@gmail.com>
2023-10-31 00:44:48 +0800

    feat: 提取xml对象

--- src/utils.ts
+++ src/utils.ts
@@ -22,6 +22,36 @@ export const decodeText = (text: string) => {
   });
 };
 
+export const isNil = (data: any) => {
+  // eslint-disable-next-line eqeqeq
+  return data == undefined;
+};
+
+const dfsResult = (record: any, temp: any[] = []) => {
+  if (Array.isArray(record)) {
+    for (const item of record) {
+      dfsResult(item, temp);
+    }
+  }
+  if (typeof record === "object" && record !== null) {
+    if (Object.keys(record).length === 0) {
+      return temp;
+    }
+    if (!isNil(record.title) && !isNil(record.url) && !isNil(record.pub_time)) {
+      temp.push({
+        title: record.title[0],
+        url: record.url[0],
+        pub_time: record.pub_time[0],
+      });
+    }
+    for (const key in record) {
+      dfsResult(record[key], temp);
+    }
+  }
+  return temp;
+};
+
 export const parseXML = async (text: string) => {
   const result = await parseStringPromise(text, { trim: true });
+  return dfsResult(result);
 };
